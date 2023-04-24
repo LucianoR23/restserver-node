0c6d6ee9efb88012015020659ce41e48d35c4d46
LucianoR23 <231097lr@gmail.com>
2023-04-24 20:34:06 -0300

    148

--- models/server.js
+++ models/server.js
@@ -8,6 +8,7 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
         this.usersPath = '/api/users';
+        this.authPath = '/api/auth';
 
         this.conectarDB();
 
@@ -35,6 +36,7 @@ class Server {
 
     routes() {
         this.app.use(this.usersPath, require('../routes/users'))
+        this.app.use(this.authPath, require('../routes/auth'))
 
     }
 
